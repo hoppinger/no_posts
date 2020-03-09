09560277f11207ceb65e416ac47ba6aae0b53da2
Sebastiaan de Geus <sebastiaan@viddadigital.com>
2020-03-09 18:04:58 +0100

    Update to WordPress 5.x

--- no-posts.php
+++ no-posts.php
@@ -1,13 +1,21 @@
 <?php
 # Plugin Name: No Posts
-# Version: 0.1.0
+# Version: 0.2.0
 # Description: WordPress plugin to disable the "post" post type.
 # Author: Sebastiaan de Geus
 # Author URI: https://www.hoppinger.com/
 # Plugin URI: https://github.com/hoppinger/no_posts
 
-require 'lib/no_posts.php';
+add_action('admin_init', function() {
+  remove_menu_page( 'edit.php' );
+  remove_submenu_page('edit-tags.php?taxonomy=category', 'edit-tags.php?taxonomy=category');
+  remove_submenu_page('edit-tags.php?taxonomy=category', 'edit-tags.php?taxonomy=post_tag');
+});
 
-$no_post = new NoPosts;
-$no_post->actions();
-$no_post->filters();
+add_action( 'admin_bar_menu', function($wp_admin_bar) {
+  $wp_admin_bar->remove_node( 'new-post' );
+});
+
+add_action( 'wp_dashboard_setup', function() {
+  remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
+});
